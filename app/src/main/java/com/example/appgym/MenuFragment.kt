e907bd3c0892034ce4d2fe4d814750bebb0dd23e
daniel <daniel.c.l@hotmail.com>
2022-04-14 13:50:07 +0200

    profile

--- app/src/main/java/com/example/appgym/MenuFragment.kt
+++ app/src/main/java/com/example/appgym/MenuFragment.kt
@@ -1,7 +1,46 @@
 package com.example.appgym
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.appgym.databinding.FragmentMenuBinding
 
 class MenuFragment : Fragment() {
+
+    private var _binding: FragmentMenuBinding? = null
+
+    private val binding get() = _binding!!
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentMenuBinding.inflate(inflater, container, false)
+        val root = binding.root
+        return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.apply {
+            menuFragment = this@MenuFragment
+        }
+    }
+
+    fun goToNextScreen(num: Int){
+        when(num){
+            1 -> findNavController().navigate(R.id.action_menuFragment_to_classFragment)
+            2 -> findNavController().navigate(R.id.action_menuFragment_to_competitionsFragment)
+            3 -> findNavController().navigate(R.id.action_menuFragment_to_profileFragment)
+        }
+    }
 
 }
