de05cc81a89b30dc74b5ff49a5162fd592571b9e
daniel <daniel.c.l@hotmail.com>
2022-04-15 12:32:48 +0200

    toolbar

--- app/src/main/java/com/example/appgym/MainActivity.kt
+++ app/src/main/java/com/example/appgym/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.appgym.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -22,10 +21,37 @@ class MainActivity : AppCompatActivity() {
             .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         navController = navHostFragment.navController
 
-        setupActionBarWithNavController(navController)
+        //setupActionBarWithNavController(navController)
+        //setupActionBarWithNavController(findNavController(R.id.fragment))
+
+        /*binding.topAppBar.apply {
+
+            setNavigationOnClickListener {
+
+            }
+
+            setOnMenuItemClickListener {
+                    menuItem ->
+
+                when (menuItem.itemId) {
+                    R.id.contactIcon -> {
+
+                        true
+                    }
+                    else -> false
+                }
+            }
+
+        }*/
+
     }
+
 
     override fun onSupportNavigateUp(): Boolean {
         return navController.navigateUp() || super.onSupportNavigateUp()
     }
+
+
+
+
 }
