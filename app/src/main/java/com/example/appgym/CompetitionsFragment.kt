d4ad1009690da5b0221dbf4c4ae1c5dcc9ff5851
daniel <daniel.c.l@hotmail.com>
2022-05-05 23:06:20 +0200

    recycler done

--- app/src/main/java/com/example/appgym/CompetitionsFragment.kt
+++ app/src/main/java/com/example/appgym/CompetitionsFragment.kt
@@ -5,14 +5,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.appgym.recycler.adapter.EventAdapter
+import kotlinx.android.synthetic.main.fragment_competitions.*
 
 
 class CompeticionesFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
+    private var layoutManager: RecyclerView.LayoutManager? = null
+    private var adapter: RecyclerView.Adapter<EventAdapter.EventViewHolder>? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,6 +22,15 @@ class CompeticionesFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_competitions, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        vertical_recycler_view.apply{
+            setHasFixedSize(true)
+            layoutManager = LinearLayoutManager(activity)
+            adapter = EventAdapter(activity)
+        }
     }
 
 
---
+++ app/src/main/java/com/example/appgym/recycler/adapter/EventAdapter.kt
@@ -0,0 +1,55 @@
+package com.example.appgym.recycler.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.appgym.R
+import com.example.appgym.recycler.data.DataSource
+
+class EventAdapter (
+    private val context: Context?
+): RecyclerView.Adapter<EventAdapter.EventViewHolder>() {
+
+
+    private val eventList = DataSource.events
+
+    class EventViewHolder(view: View?): RecyclerView.ViewHolder(view!!) {
+        val eventImageView : ImageView? = view?.findViewById(R.id.event_imageView)
+        val eventNameText : TextView? = view?.findViewById(R.id.event_textView)
+        val eventDateText : TextView? = view?.findViewById(R.id.date_textView)
+        val eventPlaceText : TextView? = view?.findViewById(R.id.place_textView)
+        val eventLevelText : TextView? = view?.findViewById(R.id.level_textView)
+        val eventContactText : TextView? = view?.findViewById(R.id.contact_textView)
+
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EventViewHolder {
+
+
+        val adapterLayout = LayoutInflater.from(parent.context).inflate(R.layout.event_list, parent, false)
+
+
+
+        return EventViewHolder(adapterLayout)
+    }
+
+
+    override fun getItemCount() = eventList.size
+
+    override fun onBindViewHolder(holder: EventViewHolder, position: Int) {
+
+        val eventData = eventList[position]
+        holder.eventImageView?.setImageResource(eventData.imageResourceId)
+        holder.eventNameText?.text  =  eventData.name
+        val resources = context?.resources
+        holder.eventDateText?.text = resources?.getString(R.string.date_event, eventData.date)
+        holder.eventPlaceText?.text = resources?.getString(R.string.place_event, eventData.place)
+        holder.eventLevelText?.text = resources?.getString(R.string.level_event, eventData.level)
+        holder.eventContactText?.text = resources?.getString(R.string.contact_event, eventData.contact)
+    }
+}
